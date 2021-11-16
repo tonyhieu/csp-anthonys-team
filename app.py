0fb028fa8eb4586509f0e84ef92053856880c625
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2021-11-15 17:29:06 -0800

    add some basic html and routing

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 
 app = Flask("app")
 
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
 @app.route("/")
 def index():
     return render_template("index.html")
