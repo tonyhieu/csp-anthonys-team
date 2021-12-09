103447a231aa03a58ade30a485fea0b361829cd0
tonyhieu <54915685+tonyhieu@users.noreply.github.com>
2021-12-09 09:33:17 -0800

    added search function and made tables clearer

--- anthony/anthony.py
+++ anthony/anthony.py
@@ -7,25 +7,28 @@ anthony_bp = Blueprint('anthony', __name__,
                    template_folder='templates',
                    static_folder='static', static_url_path='static/anthony')
 
-@anthony_bp.route("/")
+@anthony_bp.route("/", methods=['GET','POST'])
 def anthony_index():
     user_dicts = [{'username':'R04'}, {'username':'ShintaroKisaragi'}, {'username':'so phan bi xoan'}]
-    # golbal variables
-    try:
-        api_key = 'RGAPI-2fbc42f9-e279-44ba-8f3f-fa878f3d9600'
-        # RGAPI-2fbc42f9-e279-44ba-8f3f-fa878f3d9600
-        watcher = LolWatcher(api_key, 5)
-        tft_watcher = TftWatcher(api_key, 5)
-        my_region = 'na1'
+    if request.form:
+        id = request.form.get("accountId")
+        if len(id) != 0:
+            user_dicts.append({'username':id})
 
-        for user in user_dicts:
+    api_key = 'RGAPI-2fbc42f9-e279-44ba-8f3f-fa878f3d9600'
+    # RGAPI-2fbc42f9-e279-44ba-8f3f-fa878f3d9600
+    watcher = LolWatcher(api_key, 5)
+    tft_watcher = TftWatcher(api_key, 5)
+    my_region = 'na1'
+
+    for user in user_dicts:
+        try:
             summoner = watcher.summoner.by_name(my_region, user['username'])
 
             user['lol_stats'] = watcher.league.by_summoner(my_region, summoner['id'])
             user['tft_stats'] = tft_watcher.league.by_summoner(my_region, summoner['id'])
-    except:
-        for user in user_dicts:
-            user['lol_stats'] = [{'queueType': 'N/A', 'rank':'N/A', 'tier': 'N/A', 'leaguePoints': 'N/A', 'wins': 0, 'losses': 1}]
+        except:
+            user['lol_stats'] = [{'queueType': 'N/A', 'rank':'N/A', 'tier': '', 'leaguePoints': 'N/A', 'wins': 0, 'losses': 1}]
             user['tft_stats'] = []
 
     mode_dict = {'RANKED_TFT_PAIRS': 'TFT Double Up', 'RANKED_SOLO_5x5':'Ranked Solo/Duo', 'RANKED_FLEX_SR': 'Ranked Flex', 'RANKED_TFT':'Ranked TFT', 'N/A':"Data not found"}
