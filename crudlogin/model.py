83a66ca18a4881b161814b4fba95f735748a0bf4
Samuelwaang <samuelwaang@gmail.com>
2022-01-12 10:56:08 -0800

    many database progress

---
+++ crudlogin/model.py
@@ -0,0 +1,88 @@
+
+from sqlalchemy.exc import IntegrityError
+from __init__ import db
+
+
+class Users(db.Model):
+    userID = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=False, nullable=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    # CRUD create/add a new record to the table
+    # returns self or None on error
+    def create(self):
+        try:
+            # creates a person object from Users(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read converts self to dictionary
+    # returns dictionary
+    def read(self):
+        return {
+            "userID": self.userID,
+            "name": self.name,
+            "email": self.email,
+            "password": self.password,
+        }
+
+    # CRUD update: updates users name, password, phone
+    # returns self
+    def update(self, name, password=""):
+        """only updates values with length"""
+        if len(name) > 0:
+            self.name = name
+        if len(password) > 0:
+            self.password = password
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+"""Database Creation and Testing section"""
+
+
+def model_tester():
+    print("--------------------------")
+    print("Seed Data for Table: users")
+    print("--------------------------")
+    db.create_all()
+    """Tester data for table"""
+    u1 = Users(name='joe', email='123@example.com', password='12345')
+
+    table = [u1]
+    for row in table:
+        try:
+            db.session.add(row)
+            db.session.commit()
+        except IntegrityError:
+            db.session.remove()
+            print(f"error {row.email}")
+
+
+def model_printer():
+    result = db.session.execute('select * from users')
+    print(result.keys())
+    for row in result:
+        print(row)
+
+
+if __name__ == "__main__":
+    model_tester()  # builds model of Users
+    model_printer()
