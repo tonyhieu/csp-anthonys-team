3cb5fbdf92929914231458a854dc74fb71fa41f0
Isaac-Le <60956151+Isaac-Le@users.noreply.github.com>
2022-02-14 11:24:00 -0800

    minor changes

--- create_task/create_task.py
+++ create_task/create_task.py
@@ -40,6 +40,13 @@ def erik():
 
 @create_task_bp.route("/spotifyfinder")
 def spotifyfinder():
+    if request.form:
+        tracks_dicts = [{'name':'Heat Waves'}, {'release_date':'ShintaroKisaragi'}, {'username':'so phan bi xoan'}]
+        artist_dicts = [{'name':'Glass Animals'}]
+        artist_id = request.form.get("artist_id")
+        if len(artist_id) != 0:
+            tracks_dicts.append({'artist':artist_id})
+
 
     cid = '8178d9fc45274bb5b0ebef69552934bb'
     secret = '54e37228eeb94540915939462917d92f'
@@ -63,28 +70,23 @@ def spotifyfinder():
     r = r.json()
     r
 
-    # client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-    # sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-    # artist_name = []
-    # track_name = []
-    # popularity = []
-    # track_id = []
-    # for i in range(0,10000,50):
-    #     track_results = sp.search(q='year:2018', type='track', limit=50, offset=i)
-    #     for i, t in enumerate(track_results['tracks']['items']):
-    #         artist_name.append(t['artists'][0]['name'])
-    #         track_name.append(t['name'])
-    #         track_id.append(t['id'])
-    #     popularity.append(t['popularity'])
-    #
-    # track_dataframe = pd.DataFrame({'artist_name' : artist_name, 'track_name' : track_name, 'track_id' : track_id, 'popularity' : popularity})
-    # print(track_dataframe.shape)
-    # track_dataframe.head()
-    #
-    # response = requests.request("GET", url)
-    # print(response.text)
-    # output = json.loads(response.text)
+
+    r = requests.get(url + 'artists/' + artist_id + '/top-tracks',
+                     headers=headers,
+                     params={'include_groups': 'toptracks', 'limit': 5})
+    d = r.json()
+
+    for toptracks in d['items']:
+        print(toptracks['name'], ' --- ', toptracks['release_date'])
+    data = []
+    tracks = []
+
+    for track in d['items']
+        track_name = track['name']
+
+
     return render_template('spotifyfinder.html')
+
 
 @create_task_bp.route("/erik_ethan")
 def erik_ethan():
